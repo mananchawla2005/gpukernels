e17ba3ca2e4592c894196d8e2386d16f57c3ad3c
Manan Chawla <mananapeejay@gmail.com>
2025-02-07 22:52:42 +0530

    added stub file

---
+++ gpu_kernels.pyi
@@ -0,0 +1,31 @@
+import torch
+from typing import Any
+
+def vec_add(A: torch.Tensor, B: torch.Tensor, C: torch.Tensor) -> None:
+    """Performs element-wise addition of two vectors on GPU.
+
+    Args:
+        A (torch.Tensor): First input tensor (float32)
+        B (torch.Tensor): Second input tensor (float32)  
+        C (torch.Tensor): Output tensor (float32)
+    """
+    pass
+
+def rgb_to_grayscale(input: torch.Tensor, output: torch.Tensor) -> None:
+    """Converts RGB image to grayscale.
+
+    Args:
+        input (torch.Tensor): Input tensor must be unsigned char, 3D (height x width x channels)
+        output (torch.Tensor): Output tensor must be unsigned char
+    """
+    pass
+
+def simple_blur(input: torch.Tensor, output: torch.Tensor, stride: int) -> None:
+    """Converts RGB image to Blurred Image.
+
+    Args:
+        input (torch.Tensor): Input tensor must be unsigned char, 3D (height x width x channels) 
+        output (torch.Tensor): Output tensor must be unsigned char
+        stride (int): Stride value for blur operation
+    """
+    pass
